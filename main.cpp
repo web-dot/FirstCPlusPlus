d181ff47e943a154db8fb3605188fe315fca7edb
web-dot <fedev.sudipto@gmail.com>
2022-10-22 11:27:15 +0530

    c2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    cout << "Hello World!";
+    cout << "Hello World!" << \n;
+    cout << "I am programming in C++";
     return 0;
 }
+
+/**
+Line 1: #include <iostream> is a header file library that lets us work with input and output
+        objects like cout. Header files add functionality to C++ programs.
+
+Line 2: using namespace std means that we can use names for objects and variables from
+        the standard library.
+Line 5: cout is a object used togather with insertion operator (<<) to output/print text.
+
+\n - escape sequence, forces cursor to change posiion to the beginning of the next line.
+
+
+
+*/
